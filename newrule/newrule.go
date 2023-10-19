7f5ca221c1903e19e1780ffbf2561f0c6ad2efab
zhangguochao <774501072@qq.com>
2023-10-19 13:33:41 +0800

    first commit

---
+++ newrule/newrule.go
@@ -0,0 +1,29 @@
+package newrule
+
+import (
+	"go/ast"
+	"go/token"
+	"strings"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "newrule",
+	Doc:  "check for 'nohup' string",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			if str, ok := n.(*ast.BasicLit); ok && str.Kind == token.STRING {
+				if strings.Contains(str.Value, "nohup") {
+					pass.Reportf(str.Pos(), "Found 'nohup' string")
+				}
+			}
+			return true
+		})
+	}
+	return nil, nil
+}
